43450a3d41d92a85f8b7c88c46fce0e1eb5116b9
nina-mir <ninamirf@gmail.com>
2023-05-25 13:34:30 -0700

    dependency maintenance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store'
-import Vuetify from 'vuetify'
-// import 'vuetify / dist / vuetify.min.css'
-
-Vue.use(Vuetify)
 
 
 
@@ -12,7 +8,6 @@ Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify: new Vuetify(),
   render: h => h(App),
   el: '#app',
   store,
